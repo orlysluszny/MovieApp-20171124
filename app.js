3c23923e65c4650fe7f288bbb8cd044fd6f8cecc
Orly Sluszny <orly.sluszny@gmail.com>
2017-11-25 00:30:03 +0200

    Router-Final!!

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ var app = angular.module("movieApp", ["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
+        templateUrl: "home.html"
+    })
+    .when("/Actor", {
         templateUrl: "actors/actor.html"
-        
+        /*controller: "actorCtrl"*/
     })
     .when("/movie", {
         templateUrl: "movie/movie.html"
